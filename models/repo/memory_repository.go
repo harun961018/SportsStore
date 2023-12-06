251582d7e17462c7268b25b5be601e742c15e903
harun961018 <lievander24@gmail.com>
2023-12-06 14:00:49 -0500

    feat:Add catregory filter functionality

--- models/repo/memory_repository.go
+++ models/repo/memory_repository.go
@@ -38,6 +38,20 @@ func (repo *MemoryRepo) GetCategories() (results []models.Category) {
 func (repo *MemoryRepo) GetProductPage(page, pageSize int) ([]models.Product, int) {
 	return getPage(repo.products, page, pageSize), len(repo.products)
 }
+func (repo *MemoryRepo) GetProductPageCategory(category int, page,
+	pageSize int) (products []models.Product, totalAvailable int) {
+	if category == 0 {
+		return repo.GetProductPage(page, pageSize)
+	} else {
+		filteredProducts := make([]models.Product, 0, len(repo.products))
+		for _, p := range repo.products {
+			if p.Category.ID == category {
+				filteredProducts = append(filteredProducts, p)
+			}
+		}
+		return getPage(filteredProducts, page, pageSize), len(filteredProducts)
+	}
+}
 func getPage(src []models.Product, page, pageSize int) []models.Product {
 	start := (page - 1) * pageSize
 	if page > 0 && len(src) > start {
